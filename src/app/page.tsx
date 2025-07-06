1d0307a538ce8d8375f1530c101c21270a1b9674
Alberto241 <jalberto.coyotecatl@gmail.com>
2025-07-06 01:12:54 -0600

    Pruebas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@
 
 export default function Home() {
   return (
-    <main className="bg-red-500 text-white h-screen flex items-center justify-center">
-      <h1 className="text-4x1 font-bold">Pruebas</h1>
+    <main>
+      <h1>Pruebas</h1>
     </main>
     /*<main className='flex min-h-screen flex-col items-center justify-betwwen p-24'>
       <Slider/>
